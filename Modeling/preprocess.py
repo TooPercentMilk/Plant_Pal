47e2f6f01432728c31827cdf948a3cfacda4b1e5
Bradley Kerr <73859763+TooPercentMilk@users.noreply.github.com>
2025-01-12 16:06:25 -0500

    Finished training function and early stopping / historian mechanism

--- Modeling/preprocess.py
+++ Modeling/preprocess.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -36,12 +37,19 @@ class CustomDataset(torch.utils.data.Dataset):
         self.ar_limits = ar_limits
         self.image_paths = [os.path.join('../Data', fname) for fname in filenames]
 
+        self.classes = os.listdir('../Data')
+        if '.DS_Store' in self.classes:
+            self.classes.remove('.DS_Store')
+        self.label_encoder = LabelEncoder()
+        self.label_encoder.fit(self.classes)
+        self.encoded_labels = self.label_encoder.transform(self.classes)
+
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        label = self.image_paths[idx].split('/')[:-1]
-        label = int(self.image_paths[idx].split('/')[-1])
+        label = self.image_paths[idx].split('/')[2]
+        label = self.label_encoder.transform([label])[0]
         image_path = self.image_paths[idx]
         image = resize_and_sharpen(image_path, self.ar_limits)
         return image, label
@@ -53,19 +61,49 @@ class Historian():
         self.train_accuracies = []
         self.val_losses = []
         self.val_accuracies = []
+        self.best_model_state = None
+        self.best_optimizer_state = None
         self.stop = early_stopping
     
-    def record(self, loss, accuracy, val_loss, val_accuracy):
+    def record(self, loss, accuracy, val_loss, val_accuracy, model_state, opt_state):
         self.losses.append(loss)
         self.accuracies.append(accuracy)
         self.val_losses.append(val_loss)
         self.val_accuracies.append(val_accuracy)
+        if val_loss < min(self.val_losses):
+            self.best_model_state = model_state
+            self.best_optimizer_state = opt_state
         if len(self.val_losses) < self.stop:
             return True
         for i in range(self.stop):
             if self.val_losses[-1 - i] > self.val_losses[-2 - i]:
+                print('Early stopping reached - terminating training loop')
                 return False
         return True
+    
+    def save_model(self):
+        best_ind = np.argmin(self.val_losses)
+        checkpoint = {
+            'model_state_dict': self.best_model_state,
+            'optimizer_state_dict': self.best_optimizer_state,
+            'loss': self.val_losses[best_ind],
+            'accuracy': self.val_accuracies[best_ind]
+        }
+        torch.save(checkpoint, '../Best_Models/best_model.pth')
+    
+    def performance(self, verbose=False):
+        if verbose:
+            print(f'Training loss: {self.losses[-1]}, Training accuracy: {self.accuracies[-1]}')
+            print(f'Validation loss: {self.val_losses[-1]}, Validation accuracy: {self.val_accuracies[-1]}')
+        return self.losses[-1], self.accuracies[-1], self.val_losses[-1], self.val_accuracies[-1]
+    
+    def final_performance(self, verbose=False):
+        best_ind = np.argmin(self.val_losses)
+        print('Training Finished - Best Model Performance:')
+        print(f'Validation loss = {self.val_losses[best_ind]}, Validation accuracy = {self.val_accuracies[best_ind]}')
+        if verbose:
+            self.plot()
+        return self.losses[best_ind], self.accuracies[best_ind], self.val_losses[best_ind], self.val_accuracies[best_ind]
     
     def plot(self):
         fig, ax = plt.subplots(2, 1, figsize=(10, 10))
